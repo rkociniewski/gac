622d93a14d57aa2bac4d87606efee2ee29cd60bd
Rafal Kociniewski <rafal.kociniewski@gmail.com>
2025-05-11 13:32:39 +0200

    Adding tooltip

--- app/src/main/java/rk/gac/ui/config/ConfigSection.kt
+++ app/src/main/java/rk/gac/ui/config/ConfigSection.kt
@@ -1,5 +1,6 @@
 package rk.gac.ui.config
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,19 +8,29 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults.rememberPlainTooltipPositionProvider
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import rk.gac.R
 import rk.gac.enums.AdditionalMode
 import rk.gac.enums.DisplayMode
@@ -36,19 +47,18 @@ fun ConfigSection(
 ) {
     val view = LocalView.current
 
-    // Additional mode pericopes
-    Text(
+    HelpLabel(
         stringResource(R.string.section_additional_mode),
-        style = MaterialTheme.typography.titleMedium
+        stringResource(R.string.tooltip_additional_mode)
     )
     ModeSelector(AdditionalMode.entries, config.additionalMode) {
         updateConfig(config.copy(additionalMode = it))
     }
 
     if (config.additionalMode == AdditionalMode.CONDITIONAL) {
-        Text(
+        HelpLabel(
             stringResource(R.string.label_word_threshold),
-            style = MaterialTheme.typography.bodyLarge
+            stringResource(R.string.tooltip_word_threshold)
         )
         WordThresholdSelector(config.wordThreshold) {
             updateConfig(config.copy(wordThreshold = it))
@@ -56,33 +66,36 @@ fun ConfigSection(
     }
 
     if (config.additionalMode != AdditionalMode.NO) {
-        ConfigSlider(stringResource(R.string.label_prev), config.prevCount) {
-            updateConfig(config.copy(prevCount = it))
-        }
-        ConfigSlider(stringResource(R.string.label_next), config.nextCount) {
-            updateConfig(config.copy(nextCount = it))
-        }
-        ConfigSlider(stringResource(R.string.label_start_fallback), config.startFallback) {
-            updateConfig(config.copy(startFallback = it))
-        }
-        ConfigSlider(stringResource(R.string.label_end_fallback), config.endFallback) {
-            updateConfig(config.copy(endFallback = it))
-        }
+        HelpLabel(stringResource(R.string.label_prev), stringResource(R.string.tooltip_prev))
+        ConfigSlider(config.prevCount) { updateConfig(config.copy(prevCount = it)) }
+        HelpLabel(stringResource(R.string.label_next), stringResource(R.string.tooltip_next))
+        ConfigSlider(config.nextCount) { updateConfig(config.copy(nextCount = it)) }
+        HelpLabel(
+            stringResource(R.string.label_start_fallback),
+            stringResource(R.string.tooltip_start_fallback)
+        )
+        ConfigSlider(config.startFallback) { updateConfig(config.copy(startFallback = it)) }
+        HelpLabel(
+            stringResource(R.string.label_end_fallback),
+            stringResource(R.string.tooltip_end_fallback)
+        )
+        ConfigSlider(config.endFallback) { updateConfig(config.copy(endFallback = it)) }
     }
 
     HorizontalDivider()
 
-    // Dark mode display mode
-    Text(
+    HelpLabel(
         stringResource(R.string.section_display_mode),
-        style = MaterialTheme.typography.titleMedium
+        stringResource(R.string.tooltip_display_mode)
     )
     ModeSelector(DisplayMode.entries, config.displayMode) {
         updateConfig(config.copy(displayMode = it))
     }
 
-    // Drawn mode
-    Text(stringResource(R.string.section_draw_mode), style = MaterialTheme.typography.titleMedium)
+    HelpLabel(
+        stringResource(R.string.section_draw_mode),
+        stringResource(R.string.tooltip_draw_mode)
+    )
     ModeSelector(DrawMode.entries, config.drawMode) {
         updateConfig(config.copy(drawMode = it))
     }
@@ -97,16 +110,46 @@ fun ConfigSection(
     ) {
         Text(stringResource(R.string.close))
     }
+}
 
-    // Automatically trigger onClose when user taps outside the dialog
-    LaunchedEffect(view) {
-        view.rootView?.viewTreeObserver?.addOnWindowFocusChangeListener { hasFocus ->
-            if (!hasFocus) {
-                onClose()
-            }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HelpLabel(label: String, tooltip: String) {
+    val tooltipState = rememberTooltipState()
+    val scope = rememberCoroutineScope()
+
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Text(label, style = MaterialTheme.typography.titleMedium)
+        Spacer(Modifier.width(4.dp))
+        TooltipBox(
+            positionProvider = rememberPlainTooltipPositionProvider(),
+            tooltip = {
+                PlainTooltip {
+                    Text(tooltip)
+                }
+            },
+            state = tooltipState,
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Info,
+                contentDescription = stringResource(R.string.tooltip_icon_description),
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier
+                    .padding(4.dp)
+                    .clickable {
+                        scope.launch {
+                            if (!tooltipState.isVisible) {
+                                tooltipState.show()
+                            } else {
+                                tooltipState.dismiss()
+                            }
+                        }
+                    }
+            )
         }
     }
 }
+
 
 @Composable
 fun <T> ModeSelector(
@@ -140,8 +183,7 @@ fun WordThresholdSelector(selected: Int, onChange: (Int) -> Unit) {
 }
 
 @Composable
-fun ConfigSlider(label: String, value: Int, onValueChange: (Int) -> Unit) {
-    Text(label, style = MaterialTheme.typography.bodyLarge)
+fun ConfigSlider(value: Int, onValueChange: (Int) -> Unit) {
     SliderWithLabel(value = value, onValueChange = onValueChange, range = 0..2)
 }
 
