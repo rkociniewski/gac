0998d986e528a4cb3e140d821e537268400854e3
Rafal Kociniewski <rafal.kociniewski@gmail.com>
2025-10-11 21:05:44 +0200

    refactor: CodeQL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "pl.rk.gac"
         minSdk = 28
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.4.2"
+        versionCode = 17
+        versionName = "1.4.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
