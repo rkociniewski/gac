7b005f61ccfd4953ca7bb126dd748407e8577279
Rafal Kociniewski <rafal.kociniewski@gmail.com>
2025-10-14 13:30:33 +0200

    update: build gradle script

--- app/src/main/kotlin/pl/rk/gac/ui/theme/Theme.kt
+++ app/src/main/kotlin/pl/rk/gac/ui/theme/Theme.kt
@@ -34,16 +34,6 @@ val DarkColorScheme = darkColorScheme(
     secondary = MagentaSecondary,
     background = Color(0xFF121212),
     surface = Color(0xFF1E1E1E),
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 /**
